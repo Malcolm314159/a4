7f83ff6f573b2b99df50261edcb5568d77a0ab59
Malcolm Littlefield <mhlittlefield@gmail.com>
2017-05-04 19:25:05 -0400

    Added search route

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/search', 'MainController@search');
 Route::get('/tunes', 'MainController@allTunes');
 
 Route::get('/addtune', function () {
